0b27a729502c241d6adcb490e639f517b540a254
doreenkam <dkamushinda@gmail.com>
2022-05-16 11:24:41 +0100

    Set up server & pointed to relevant routes

--- app.js
+++ app.js
@@ -1,39 +1,22 @@
-const express = require('express');
-const cors = require('cors');
-const favicon = require('express-favicon');
+const express = require("express");
 
 const app = express();
+const cors = require("cors");
 
-app.use(cors("*"));
+app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.send("Welcome to the Neweet Trivia API!");
+});
 
-const helloRoutes = require('./routes/helloRoutes');
-const rootRoute = require('./routes/rootRoute');
+const userRoutes = require("./routes/userRoutes");
+app.use("/api/users", userRoutes);
 
-app.set('view engine', 'ejs');
-
-app.use(favicon(__dirname + '/public/images/favicon.png'));
-
-app.get('/', (req, res) => {
-    res.json({
-        root_endpoint: "http://localhost:3000/api",
-    })
-})
-
-//app.get('*', checkUser); // Middleware that applies to every endpoint
-
-// Routes 
-app.use('/api/hello', helloRoutes); 
-app.use('/api', rootRoute); // Routes 
-
-
-// 404 page if user naviagtes to endpoint that does not exist
+// 404 page if user navigatesto non-existent endpoint
 app.use((req, res) => {
-    res.status(404).render('404')
-})
+  res.status(404).render("404");
+});
 
-//exporting
-module.exports = app
+module.exports = app;
