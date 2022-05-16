a3c3eb753ae7a1eae1d593bf05f7028b52d3b805
jianli1028 <96356376+jianli1028@users.noreply.github.com>
2022-05-16 15:44:48 +0100

    server and API connected

--- controllers/allRecordController.js
+++ controllers/allRecordController.js
@@ -1,16 +1,25 @@
 //get all records
-const RecordModel = require("../models/neweetRecord");
+const recordSchema = require("../models/neweetRecord");
 
-function showAllRecords(req, res) {
-    RecordModel.find({}, (err, result) => {
-        try {
-            res.json(result);
-            res.status(200);
-        } catch (err) {
-            console.log(err);
-            res.status(404).json({ err });
-        }
-    });
+// function showAllRecords(req, res) {
+//     recordSchema.RecordModel.find({}, (err, result) => {
+//         try {
+//             res.json(result);
+//             res.status(200);
+//         } catch (err) {
+//             console.log(err);
+//             res.status(404).json({ err });
+//         }
+//     });
+// }
+
+ async function showAllRecords(req, res) {
+    try {
+        const records = await recordSchema.RecordModel.find();
+        res.json(records);
+    } catch (err) {
+        res.status(404).json("Error 404 (Not found)");
+    }
 }
 
 module.exports = { showAllRecords }
